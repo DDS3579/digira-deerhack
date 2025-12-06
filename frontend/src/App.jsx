7a8f7744a2f8dac1e89ce81e0dc477eb4c307352
Divya <divyadarsheel.dds@gmail.com>
2025-12-06 14:16:14 +0545

    Time: 2:20pm, remaining time 3 hour 40 minutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import { Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "@/components/auth/Login.jsx";
 import RegisterUser from "@/components/auth/RegisterUser.jsx";
 import RegisterAdmin from "@/components/auth/RegisterAdmin.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AdminLayout from "./components/layout/AdminLayout";
-
-function RoleRedirect() {}
+import AdminDashboard from "./components/admin/AdminDashboard";
+import ViewComplaints from "./components/admin/ViewComplaints";
+import Samachar from "./components/admin/Samachar";
+import Events from "./components/admin/Events";
 
 function App() {
   return (
@@ -16,8 +18,17 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/registerUser" element={<RegisterUser />} />
         <Route path="/registerAdmin" element={<RegisterAdmin />} />
+        
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Navigate to="/admin/dashboard" replace />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="complaints" element={<ViewComplaints />} />
+          <Route path="samachar" element={<Samachar />} />
+          <Route path="events" element={<Events />} />
+        </Route>
+        
         <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="/admin" element={<AdminLayout />} />
       </Routes>
     </BrowserRouter>
   );
